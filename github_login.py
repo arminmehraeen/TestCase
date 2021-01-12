1c969c43878d0a80fcee18b6a048aa19f9995570
armin <armin.mehraeen@gmail.com>
2021-01-12 12:08:56 +0330

    first commit

---
+++ github_login.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe')
+driver.get('https://github.com/login')
+
+elem = driver.find_element_by_name("login")
+elem.clear()
+elem.send_keys("example@gamil.com")
+
+elem = driver.find_element_by_name("password")
+elem.clear()
+
+elem = driver.find_element_by_name("commit")
+elem.click()
+time.sleep(5)
+
+elem = driver.find_element_by_name("login")
+elem.clear()
+
+elem = driver.find_element_by_name("password")
+elem.clear()
+elem.send_keys("12345678")
+
+elem = driver.find_element_by_name("commit")
+elem.click()
+time.sleep(5)
+
+elem = driver.find_element_by_name("login")
+elem.clear()
+elem.send_keys("example@gamil.com")
+
+elem = driver.find_element_by_name("password")
+elem.clear()
+elem.send_keys("12345678")
+
+elem = driver.find_element_by_name("commit")
+elem.click()
+time.sleep(5)
+
+assert "no result found ." not in driver.page_source
